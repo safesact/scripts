3b06613390b7f3c60618dbb51a9bf3ef49d3ff8c
Suraj Shamsundar Jain <surajsjain@hotmail.com>
2021-12-04 18:16:50 -0600

    Script to predict the fraud using the model

---
+++ machine_learning/fraud_predictor.py
@@ -0,0 +1,12 @@
+import pickle
+from .btc_dataloader import get_account_summary
+import numpy as np
+
+model_path = "models/rfc_bitcoin.bin"
+
+def predict_if_btc_fraud(btc_address):
+    rfc_clf = pickle.load(open(model_path, 'rb'))
+    in_data = get_account_summary(btc_address)
+    pred = rfc_clf.predict(np.array(in_data).reshape(1, -1))
+
+    return True if pred == 1 else False
