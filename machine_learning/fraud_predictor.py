051e975a89023add0dec8a9f3ee47ef50aaac07b
Suraj Shamsundar Jain <surajsjain@hotmail.com>
2021-12-07 18:54:18 -0600

    Checking multiple addresses possible

--- machine_learning/fraud_predictor.py
+++ machine_learning/fraud_predictor.py
@@ -10,3 +10,10 @@ def predict_if_btc_fraud(btc_address):
     pred = rfc_clf.predict(np.array(in_data).reshape(1, -1))
 
     return True if pred == 1 else False
+
+def predict_if_btc_fraud_with_model(btc_address, model):
+    rfc_clf = model
+    in_data = get_account_summary(btc_address)
+    pred = rfc_clf.predict(np.array(in_data).reshape(1, -1))
+
+    return True if pred == 1 else False
